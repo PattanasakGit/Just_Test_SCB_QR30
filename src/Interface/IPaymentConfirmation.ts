78511413b37ff0a141e991b5463ca8225876b8b7
Just Pattanasak <pattanasak@nipa.cloud>
2023-12-06 12:16:22 +0700

    update IPaymentConfirmation

--- src/Interface/IPaymentConfirmation.ts
+++ src/Interface/IPaymentConfirmation.ts
@@ -2,18 +2,20 @@ export interface IPaymentConfirmation {
   payeeProxyId: string;
   payeeProxyType: string;
   payeeAccountNumber: string;
+  payeeName: string; 
+  payerProxyId: string; 
+  payerProxyType: string; 
   payerAccountNumber: string;
-  payerAccountName: string;
   payerName: string;
   sendingBankCode: string;
   receivingBankCode: string;
   amount: string;
+  channelCode: string;
   transactionId: string;
   transactionDateandTime: string;
   billPaymentRef1: string;
   billPaymentRef2: string;
   billPaymentRef3: string;
   currencyCode: string;
-  channelCode: string;
   transactionType: string;
 }
